1ee9a95a7461de9f88bf4695a7833dd89e0ceae0
raghavbansal9518-cloud <bansalraghav9518@gmail.com>
2026-08-04 20:15:37 +0530

    Time: 1 ms (99.95%), Space: 46.9 MB (46.69%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,22 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            boolean output=true;
-            for(int j=0;j<nums.length;j++){
-            if(i!=j&&nums[i]==nums[j]){
-                output=false;
-                break;
-            }
+        // for(int i=0;i<nums.length;i++){
+        //     boolean output=true;
+        //     for(int j=0;j<nums.length;j++){
+        //     if(i!=j&&nums[i]==nums[j]){
+        //         output=false;
+        //         break;
+        //     }
+        // }
+        // if(output){
+        //     return nums[i];
+        // }
+        // }
+        // return -1;
+        int res=0;
+        for(int num:nums){
+            res^=num;
         }
-        if(output){
-            return nums[i];
-        }
-        }
-        return -1;
+        return res;
     }
 }
