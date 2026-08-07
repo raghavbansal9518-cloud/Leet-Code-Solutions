b23ae8b2ce77c98ff38579d97cb4690660d38daf
raghavbansal9518-cloud <bansalraghav9518@gmail.com>
2026-08-07 16:58:22 +0530

    Time: 0 ms (100%), Space: 44.4 MB (17.46%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int sum=0;
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            nums[k]=sum;
+            k++;
+        }
+        return nums;
+    }
+}
