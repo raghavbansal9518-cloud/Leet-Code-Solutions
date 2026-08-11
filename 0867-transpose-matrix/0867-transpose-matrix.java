db1c9890d1f155f4ec8a32fe5d02c4464cff435a
raghavbansal9518-cloud <bansalraghav9518@gmail.com>
2026-08-12 00:08:27 +0530

    Time: 0 ms (100%), Space: 47 MB (11.84%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] transpose(int[][] m) {
+        int row=m.length;
+        int col=m[0].length;
+        int[][] t=new int[col][row];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                t[j][i]=m[i][j];
+            }
+        }
+        return t;
+    }
+}
