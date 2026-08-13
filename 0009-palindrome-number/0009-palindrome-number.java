ed61d824146e6f34b98fb3bbfde975c3df0d1c03
raghavbansal9518-cloud <bansalraghav9518@gmail.com>
2026-08-13 20:47:17 +0530

    Time: 4 ms (100%), Space: 45.9 MB (72.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int org=x;
         int rev=0;
+        int org=x;
         if(x<0){
             return false;
         }
         while(x>0){
             rev=rev*10+x%10;
-            x=x/10;
+            x/=10;
         }
-        return rev==org;
+        if(rev==org)return true;
+        return false;
     }
 }
