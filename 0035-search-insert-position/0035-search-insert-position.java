cdb34517b3fc844a84d29513992f324ebc9c62b0
raghavbansal9518-cloud <bansalraghav9518@gmail.com>
2026-08-14 18:28:15 +0530

    Time: 0 ms (100%), Space: 44.3 MB (96.65%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -14,11 +14,27 @@ class Solution {
         //     }
         // }
         // return 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=target){
-                return i;
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]>=target){
+        //         return i;
+        //     }
+        // }
+        // return nums.length;
+
+        int start=0;
+        int end=nums.length-1;
+        int ans=nums.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
             }
         }
-        return nums.length;
+        return ans;
     }
 }
