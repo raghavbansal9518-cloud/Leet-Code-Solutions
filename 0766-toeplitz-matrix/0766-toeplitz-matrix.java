2302aa61e8f67bdc2bcb702290f73f3f83de7bb6
raghavbansal9518-cloud <bansalraghav9518@gmail.com>
2026-08-25 14:01:36 +0530

    Time: 0 ms (100%), Space: 46.5 MB (47.02%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,14 +1,17 @@
 class Solution {
-    public boolean isToeplitzMatrix(int[][] matrix) {
-        int row=matrix.length;
-        int col=matrix[0].length;
+    public boolean isToeplitzMatrix(int[][] m) {
+        int row=m.length;
+        int col=m[0].length;
+        boolean equal=true;
         for(int i=0;i<row-1;i++){
             for(int j=0;j<col-1;j++){
-                if(matrix[i][j]!=matrix[i+1][j+1]){
-                    return false;
+                if(m[i][j]!=m[i+1][j+1]){
+                    equal=false;
+                    break;
                 }
             }
+            if(!equal)break;
         }
-        return true;
+        return equal;
     }
 }
