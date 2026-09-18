a1bace50d95eb2cf7f363eb87f268ad20731a375
raghavbansal9518-cloud <bansalraghav9518@gmail.com>
2026-09-18 23:48:17 +0530

    Time: 0 ms (100%), Space: 44.1 MB (16.44%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] m) {
+        for(int i=0;i<m.length;i++){
+            for(int j=i;j<m.length;j++){
+                int temp=m[i][j];
+                m[i][j]=m[j][i];
+                m[j][i]=temp;
+            }
+        }
+        for(int i=0;i<m.length;i++){
+            int left=0,right=m.length-1;
+            while(left<right){
+                int temp=m[i][left];
+                m[i][left]=m[i][right];
+                m[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
